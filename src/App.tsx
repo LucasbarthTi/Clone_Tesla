327b7f35b1a7acfc96ce6ff263e1a0f49a500f8b
Lucas_Barth <lucasbarth@ienh.com.br>
2021-04-24 19:35:35 -0300

    Clone da interface do site da tesla em react

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Page from './components/Page'
+
+import { GlobalStyle } from './style/GlobalStyles';
+
+function App() {
+  return (
+    <>
+    
+    <Page />
+
+      <GlobalStyle />
+    </>
+  )
+}
+
+export default App;
